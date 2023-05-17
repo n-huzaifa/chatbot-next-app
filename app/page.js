cbd2c77c14e6b02cbe75e685d41116b75709ea53
noorhuzaifa <n.huzaifa@gridship.io>
2023-05-17 15:42:24 +0500

    chatbot init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,83 @@
-import Image from 'next/image'
+'use client'
+import React, { useState } from 'react'
 
-export default function Home() {
+const responses = {
+  'What is a software architecture?':
+    "Software architecture is the process of designing and defining the structure of a software system to meet the system's requirements and ensure it can be maintained and evolved over time.",
+  'What is a system architecture?':
+    "System architecture refers to the overall design of a system, including its structure, components, and their interactions, to meet the system's functional and non-functional requirements.",
+  'What is the difference between a monolithic and a microservices architecture?':
+    'A monolithic architecture is a single, self-contained application with all its functionality tightly coupled, while a microservices architecture is a collection of independent services that communicate with each other through APIs.',
+  'What is a design pattern?':
+    'A design pattern is a reusable solution to a commonly occurring problem in software design, providing a way to solve problems more efficiently and effectively.',
+  'What is a layered architecture?':
+    'A layered architecture divides a system into distinct layers, each responsible for a specific set of functionality, facilitating modularization and separation of concerns.',
+  'What is an event-driven architecture?':
+    'An event-driven architecture is a design approach where software components communicate with each other by producing and consuming events, providing loose coupling and scalability.',
+  'What is an API?':
+    'An API (Application Programming Interface) is a set of rules and protocols for accessing a software application or web-based system, enabling different software systems to communicate with each other.',
+  'What is a RESTful API?':
+    'A RESTful API is an API that follows the principles of Representational State Transfer (REST), providing a standardized way to interact with web-based systems using HTTP protocols.',
+  'What is a service-oriented architecture?':
+    'A service-oriented architecture (SOA) is a design approach where software components are designed as loosely coupled, interoperable services that can be reused across different applications and platforms.',
+  'What is a domain-driven design?':
+    'Domain-driven design (DDD) is an approach to software development that emphasizes understanding and modeling the problem domain to create a more effective software solution.',
+  'What is a component-based architecture?':
+    'A component-based architecture is a design approach where software components are designed as independent, reusable modules that can be assembled to form larger systems.',
+  'What is a container?':
+    'A container is a lightweight, standalone executable package that contains all the dependencies and configuration needed to run an application, providing a consistent and portable environment.',
+  'What is a message queue?':
+    'A message queue is a mechanism for asynchronous communication between software components, allowing messages to be sent and received without direct coupling or dependency.',
+  'What is an architecture style?':
+    'An architecture style is a set of principles, patterns, and practices that define a particular approach to designing software architecture, such as client-server, peer-to-peer, or event-driven.',
+  'What is a data flow diagram?':
+    "A data flow diagram is a graphical representation of a system's processes, inputs, outputs, and data flows, used to model and understand the system's functionality.",
+  'What is a sequence diagram?':
+    'A sequence diagram is a visual representation of the interactions between software components in a system, illustrating the sequence of messages exchanged between them.',
+  'What is an architectural pattern?':
+    'An architectural pattern is a general, reusable solution to a commonly occurring problem in software architecture, providing a way to address common design challenges more effectively and efficiently.',
+  'What is a deployment diagram?':
+    'A deployment diagram is a visual representation of the physical architecture of a system, illustrating how software components and hardware devices are deployed and connected.',
+  'What is an architectural style?':
+    'An architectural style is a specific way of organizing and structuring the components, modules, and relationships in a software system, such as layered, client-server, or event-driven.',
+  'What is a scalability?':
+    'Scalability is the ability of a system to handle increasing workloads and accommodate growth, either by scaling up (vertical scaling) or scaling out (horizontal scaling).',
+}
+
+export default function Chatbot() {
+  const [userInput, setUserInput] = useState('')
+  const [chatHistory, setChatHistory] = useState([])
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const userQuery = userInput.trim()
+    const chatResponse = responses[userQuery] || "I'm sorry, I couldn't understand your question. Please try again."
+
+    setChatHistory([...chatHistory, { userQuery, chatResponse }])
+    setUserInput('')
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <h1>Chatbot</h1>
+      <div className='chat-history'>
+        {chatHistory.map((entry, index) => (
+          <div key={index} className='chat-entry'>
+            <strong>User:</strong> {entry.userQuery}
+            <br />
+            <strong>Chatbot:</strong> {entry.chatResponse}
+            <hr />
+          </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={userInput} onChange={handleChange} />
+        <button type='submit'>Send</button>
+      </form>
+    </div>
   )
 }
